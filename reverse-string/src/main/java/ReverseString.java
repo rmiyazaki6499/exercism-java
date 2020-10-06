79c2e740d1637fed25d64da6735b33f158babb31
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-10-06 16:11:16 -0700

    First try at Reverse String

--- reverse-string/src/main/java/ReverseString.java
+++ reverse-string/src/main/java/ReverseString.java
@@ -1,7 +1,12 @@
 class ReverseString {
 
     String reverse(String inputString) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (inputString == null) return null
+        String reversedString ='';
+        for (int i = inputString.length() - 1; i >= 0; i--) {
+            reversedString += inputString[i];
+        }
+        return reversedString;
     }
   
 }

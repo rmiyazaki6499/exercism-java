e962e378dda037edbc53acd627b0fa4bc310950c
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-10-07 21:40:13 +0000

    First try at Leap year

--- leap/src/main/java/Leap.java
+++ leap/src/main/java/Leap.java
@@ -1,7 +1,17 @@
 class Leap {
 
     boolean isLeapYear(int year) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+        boolean leap = false;
 
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    leap = true;
+                }
+            }
+            else
+                leap = true;
+        }
+        return leap;
+    }
 }
